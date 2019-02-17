a4309b885fd8ca165c290adb63da862a4c84804e
Kousilla <rkarpov90@gmail.com>
2019-02-16 21:57:32 -0500

    add close icon to remove topics

--- frontend/components/topic/topic_index_item.jsx
+++ frontend/components/topic/topic_index_item.jsx
@@ -2,27 +2,6 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 // import TopicIndexContainer from './topic_index_container';
 class TopicIndexItem extends React.Component {
-   
-    // doSomething(){
-    //     debugger
-    //     let topicIdss = []
-    //     this.props.topicIds.forEach(topicId => {
-    //         debugger
-    //         if (this.props.topic.id != topicId) { topicIdss.push(topicId)}
-    //     })
-    //     this.props.deleteTopic({ topic_id: this.props.topic.id, question_id: this.props.questionId })
-        // this.forceUpdate()
-        // this.setState(this.state)
-        // return (
-        // < TopicIndexContainer
-        //     sourceType={'fuck'}
-        //     topicIds={this.topicIdss}
-        //     questionId={this.props.questionId}
-        // />  )
-        // this.setProps(this.props.sourceType = 'ignore')
-        // // this.props.requestTopics();
-        // // this.props.fetchQuestion(this.props.questionId)
-    // }
  
     render() {
         return(
@@ -31,16 +10,19 @@ class TopicIndexItem extends React.Component {
                 <Link to={`/api/topics/${this.props.topic.description}`}>
                     { this.props.topic.description } 
                 </Link>
-                <div hidden={this.props.match.url.includes('question') ? null : "hidden"}>
-                <button 
-                    className="delete-topic-button"
-                    // onClick={() => this.props.deleteTopic(this.props.topic.id)}
-                    onClick={() => this.props.deleteTopic({ topic_id: this.props.topic.id, question_id: this.props.questionId })}
-                    // onClick={() => this.doSomething()}
-                    hidden={this.props.sourceType === 'feedIndex' ? "hidden" : null }    
-                    >
-                x
-                </button>
+                <div className="delete-topic-button-container" hidden={this.props.match.url.includes('question') ? null : "hidden"}>
+                    <button 
+                        className="delete-topic-button"
+                        // src={window.closeIcon}
+                        
+                        // onClick={() => this.props.deleteTopic(this.props.topic.id)}
+                        onClick={() => this.props.deleteTopic({ topic_id: this.props.topic.id, question_id: this.props.questionId })}
+                        // onClick={() => this.doSomething()}
+                        hidden={this.props.sourceType === 'feedIndex' ? "hidden" : null }    
+                        >
+                    {/* x */}
+                        <img src={window.closeIcon} />
+                    </button>
                 </div>
             </div>
         )
