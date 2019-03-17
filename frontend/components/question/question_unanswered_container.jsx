ca05afdb8800d76e7a4c359d93d14341a7dd60b8
Kousilla <rkarpov90@gmail.com>
2019-03-17 12:30:09 -0400

    pass appropriate fetch methods to component

--- frontend/components/question/question_unanswered_container.jsx
+++ frontend/components/question/question_unanswered_container.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { requestQuestions, deleteQuestion } from '../../actions/question_actions';
+import { requestTopics } from '../../actions/topic_actions';
+import { requestAnswers } from '../../actions/answer_actions';
+import { requestComments } from '../../actions/comment_actions';
 import { createQuestionVote } from '../../actions/vote_actions';
 
 import QuestionIndex from './question_index';
@@ -18,6 +21,9 @@ const mdp = dispatch => {
     return {
         requestQuestions: () => dispatch(requestQuestions()),
         deleteQuestion: (questionId) => dispatch(deleteQuestion(questionId)),
+        requestAnswers: () => dispatch(requestAnswers()),
+        requestComments: () => dispatch(requestComments()),
+        requestTopics: () => dispatch(requestTopics()),
         createQuestionVote: (vote) => dispatch(createQuestionVote(vote)),
         openModal: (modal) => dispatch(openModal(modal)),
     }
